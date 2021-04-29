038ed9279ddb6855206eda6c69f911588f987e96
kabelo masemola <kabelo.masemola@sambe.co.za>
2021-04-29 20:49:20 +0200

    Started response cleanup

---
+++ sego/Response/Response.py
@@ -0,0 +1,7 @@
+# ************************************************************************#
+# Title:                    Sego Response API                             #
+# Description:              This class handles response objects           #
+# Author:                   Sambe Consulting <development@sambe.co.za>    #
+# Original Date:            20 March 2021                                 #
+# Version:                  0.1.0                                         #
+# ************************************************************************#
--- sego/tests/test_sego.py
+++ sego/tests/test_sego.py
@@ -77,20 +77,3 @@ def test_parameterized_route(sego, client):
     assert client.get("http://segotestserver/kgotso").text == "hey kgotso"
 
 
-# def test_middleware_methods_are_called(sego, client):
-#     process_request_called = False
-#     process_response_called = False
-#
-#     class CallMiddlewareMethods(Middleware):
-#         def __init__(self, app):
-#             super().__init__(app)
-#
-#         def process_request(self, req):
-#             nonlocal process_request_called
-#             process_request_called = True
-#
-#         def process_response(self, req, resp):
-#             nonlocal process_response_called
-#             process_response_called = True
-#
-#     sego.add_middleware(CallMiddlewareMethods)
