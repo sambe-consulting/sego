17f513195d1e4ef3c701051fedbe87820b1f6ad6
kabelo masemola <kabelo.masemola@sambe.co.za>
2021-06-01 21:34:45 +0200

    added session handling

--- sego/Response/Response.py
+++ sego/Response/Response.py
@@ -22,6 +22,9 @@ class Response:
         self.content_type = None
         self.body = b''
         self.status_code = 200
+        self.cookies = []
+        self.delete_cookies = []
+        self.unset_cookies = []
 
     def __call__(self, environ, start_response):
         self.set_body_and_content_type()
@@ -29,6 +32,7 @@ class Response:
             body=self.body, content_type=self.content_type, status=self.status_code
         )
 
+        response = self.build_cookies(response=response)
         if self.location is not None:
             response.location = self.location
 
@@ -50,3 +54,31 @@ class Response:
         if self.location is not None:
            self.status_code = 308
 
+    def set_cookie(self, name, value='', max_age=None,
+                   path='/', domain=None, secure=False, httponly=False,
+                   comment=None, expires=None, overwrite=False,
+                   samesite=None):
+        cur_cookie = {
+            "name":name,"value":value,"max_age":max_age,"path":path,
+            "domain":domain,"secure":secure,"httponly":httponly,"comment":comment,
+            "expires":expires,"overwrite":overwrite,"samesite":samesite
+        }
+        self.cookies.append(cur_cookie)
+
+    def delete_cookie(self,name, path='/', domain=None):
+        self.delete_cookies.append({"name":name,"path":path,"domain":domain})
+
+    def unset_cookie(self, name, strict=True):
+        self.unset_cookies.append({"name":name,"strict":strict})
+
+    def build_cookies(self,response):
+        for cookie in self.cookies:
+            response.set_cookie(**cookie)
+
+        for cookie in self.delete_cookies:
+            response.delete_cookie(**cookie)
+
+        for cookie in self.unset_cookies:
+            response.unset_cookie(**cookie)
+
+        return response
